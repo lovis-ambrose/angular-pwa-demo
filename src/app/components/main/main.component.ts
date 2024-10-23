a5da0e70a97821965c0d1607d6f5becdada780ec
lovis-ambrose <lovisambrose212@gmail.com>
2024-10-23 12:26:53 +0300

    implement main template

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'bb-main',
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
-export class MainComponent {
-
+export class MainComponent{
 }
